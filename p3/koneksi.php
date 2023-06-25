aec3ce93542f5704d557ac27db65f92cf7b3699e
Mas Sahal <abusahl444@gmail.com>
2023-06-25 20:39:13 +0700

    first - v2

--- p3/koneksi.php
+++ p3/koneksi.php
@@ -1,17 +1,14 @@
 <?php
-$servername = "localhost"; // Replace with your MySQL server name
-$username = "roots"; // Replace with your MySQL username
-$password = ""; // Replace with your MySQL password
+$servername = "localhost";
+$username = "root";
+$password = "";
 
-// Create a connection
 $conn = mysqli_connect($servername, $username, $password);
 
-// Check the connection
 if (!$conn) {
     echo "error";
 } else {
     echo "success";
 }
 
-// Close the connection
 $conn->close();
